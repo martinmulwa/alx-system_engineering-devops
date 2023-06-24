80c7d7212b82ac559d3b3a3f034ae23705a48e11
Martin Pius <martinmulwa1711@gmail.com>
2023-06-24 15:09:12 +0300

    task: Add task 0 solution

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This manifest creates a file in /tmp with specific permissions, owner, and group
+
+file { '/tmp/school':
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
